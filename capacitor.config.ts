f849ff8f44d8e2a5fd767165efe07dc182e39d9d
montsp <konbuda4@icloud.com>
2022-05-18 06:10:36 +0200

    first

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'montsp.me-redirect',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
